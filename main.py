46ec2b114b117b4bbf58a059efe794e7263f1f4a
Eterxity <57925040+Fad3dAY@users.noreply.github.com>
2021-12-09 18:11:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-#Why yes they do
+from bs4 import BeautifulSoup
+from html.parser import HTMLParser
+import requests
+import csv
+import pandas as pd
 
+URL = "https://en.wikipedia.org/wiki/Donald_Trump"
+html_code = requests.get(URL)
 
+soup = BeautifulSoup(html_code.content, 'html5lib')
+soup2 = BeautifulSoup(html_code.text, "html.parser")
+table_id = "infobox_vcard"
+soup_table = soup.find('table', {'class':table_id})
 
+trump_info = soup2.find("table", attrs={"id" : soup_table})
 
+df = pd.read_html(str(trump_info))
 
+title = soup.title
 
+table_print = trump_info
+table_pretty = table_print.prettify()
 
+print(table_print.find_all('p'))
+
+print("END")
 
